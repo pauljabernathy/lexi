c4d6dd6e4a458acbd4f2d43c909b831133f9a76e
pauljabernathy <pauljabernathy@gmail.com>
2021-07-22 08:33:09 -0400

    refactor flask_ui and use fyj doc made from es_core_news_md

--- corpus_repo.py
+++ corpus_repo.py
@@ -1,12 +1,13 @@
 import pickle
 
+
 class CorpusRepo:
 
     def __init__(self):
-        with open("fyj/fyj_lemma_map.pkl", 'rb') as f:
+        with open("fyj/fyj_lm_md.pkl", 'rb') as f:
             self.fyj_lm = pickle.load(f)
 
-        with open('fyj/fyj.pkl', 'rb') as f:
+        with open('fyj/fyj_md.pkl', 'rb') as f:
             self.fyj = pickle.load(f)
 
         self.fyj_sentences = list(self.fyj.sents)
