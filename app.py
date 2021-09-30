08258f1365e5d314375ad483ff438218f40687f9
pauljabernathy <pauljabernathy@gmail.com>
2021-09-30 08:24:25 -0400

    adding max_examples; removing some unused code

--- app.py
+++ app.py
@@ -22,17 +22,15 @@ def lemma():
 
 def get_sentences(request):
     args = request.args
-    filters = {}
     if 'lemma' in args:
         lemma_searching_for = args['lemma']
     else:
         lemma_searching_for = None
-    for k in args.keys():
-        if 'name' in k and k.replace('name', 'value') in args.keys() and args[k] is not None and args[k] != '':
-            filters[args[k]] = args[k.replace('name', 'value')]
-    # sentences = lu.search_sentences_for_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for, filters)
-    # using lu.get_examples for the time being to avoid using the huge spacy files; functionality will be reduced
-    sentences = lu.get_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for)
+    if 'max_examples' in args:
+        max_examples = int(args['max_examples'])
+    else:
+        max_examples = lu.DEFAULT_MAX_EXAMPLES
+    sentences = lu.get_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for, max_examples)
     # sentences = lu.get_examples(cp.moby_sentences, cp.moby_lm, lemma_searching_for)
     output = ''
     for i in range(len(sentences)):
