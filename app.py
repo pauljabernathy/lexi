4be954651e375ccdbc95d86958a9ad19c800ceb1
pauljabernathy <pauljabernathy@gmail.com>
2021-09-30 08:51:53 -0400

    abiilty to select the source, and some explanatory text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import corpus_repo
 
 app = Flask(__name__)
 cp = corpus_repo.CorpusRepo()
-
+DEFAULT_SOURCE = 'fyj'
 
 @app.route("/")
 def home():
@@ -30,8 +30,16 @@ def get_sentences(request):
         max_examples = int(args['max_examples'])
     else:
         max_examples = lu.DEFAULT_MAX_EXAMPLES
-    sentences = lu.get_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for, max_examples)
-    # sentences = lu.get_examples(cp.moby_sentences, cp.moby_lm, lemma_searching_for)
+    if 'source' in args:
+        source = args['source']
+    else:
+        source = DEFAULT_SOURCE
+    if source == 'md':
+        sentences = lu.get_examples(cp.moby_sentences, cp.moby_lm, lemma_searching_for)
+    elif source == 'fyj':
+        sentences = lu.get_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for, max_examples)
+    else:
+        sentences = lu.get_examples(cp.fyj_sentences, cp.fyj_lm, lemma_searching_for, max_examples)
     output = ''
     for i in range(len(sentences)):
         output += str(i) + '. ' + sentences[i] + '\n\n'
